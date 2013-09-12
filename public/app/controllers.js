9cffa2d61bf9427ffe32c6962646382b4fab54e5
Vaso <vaso.bolkvadze@gmail.com>
2013-09-12 20:13:39 +0400

    shevkarit cre

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -5,15 +5,22 @@ function HomeController($scope,$http){
             $scope.facets = data;
         });
     $scope.apply = function(key,range){
-        var results = $scope.facets.map(function(f){
-            return f.values.filter(function(v){
-                return v.isSelected || (f.key == key && v.Range == range);
-            }).map(function(v){
-                return {r: f.key, l: v.Range};
+        var results = $scope.facets
+            .map(function(f){
+                f.values = f.values
+                    .filter(function(v){
+                        return v.isSelected || (f.key == key && v.Range == range);
+                    }).map(function(v){
+                        return v.Range;
+                    });
+                return f;
+            }).filter(function(a){
+                return a.values.length > 0;
             });
-        }).filter(function(a){
-                return a.length > 0;
+        $http({method:'POST',url:'/api/facet',data:{query:results}})
+            .success(function(data){
+                $scope.facets = data;
+                $scope.$emit('filterRequest',results);
             });
-        $scope.$emit('filterRequest',results);
     };
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     http = require('http'),
     _ = require('underscore'),
+    request = require('request'),
     session = require('ravendb').openSession("http://office.anvol.ge:8080/", "Anvol"),
     path = require('path');
 var app = module.exports = express();
@@ -28,10 +29,9 @@ app.get('/partials/:name', function(req,res){
 });
 /*----------------------*/
 /*-----Data Api------*/
-app.get('/api/facet',function(req,res){
-    session.facets("refebi","facets/refs","",function(err,results){
+function getFacetData(where,cb){
+    getFacets("http://office.anvol.ge:8080/","Anvol","refebi","facets/refs",where,function(err,results){
         var r = _.map(results,function(value,key){
-
             return {
                 key:key,
                 name:key,
@@ -39,8 +39,33 @@ app.get('/api/facet',function(req,res){
                 values:value.Values
             };
         });
-        res.json(r);
+        if(err){
+            cb(err);
+        }else{
+            cb(null,r);
+        }
     });
+}
+app.get('/api/facet',function(req,res){
+    getFacetData("",function(err,result){
+        res.json(result);
+    })
+});
+app.post('/api/facet',function(req,res){
+    var whereClause =  req.body.query
+        .filter(function(f){
+            return !f.isMulti;
+        })
+        .map(function(f){
+            return f.values.map(function(v){
+                return f.name + ":" + v;
+            }).join(" OR ");
+        }).join(" AND ");
+    console.log(whereClause);
+    getFacetData(whereClause,function(err,result){
+        console.log(err,result);
+        res.json(result);
+    })
 });
 /*-------------------*/
 
@@ -48,3 +73,15 @@ app.get('/api/facet',function(req,res){
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+function getFacets(host,db,index, facetDoc, query, cb) {
+    var url = host + "databases/" + db + "/facets/" + index + "?facetDoc=" + facetDoc + "&query=" + query + "&pageSize=128";
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var result = JSON.parse(body);
+            cb(null,result.Results)
+        } else {
+            cb(error || response.statusCode, null);
+        }
+    });
+};
